081d24a0ae0a5e6552d112b3e144fac097dbd7d8
Balachonov <balachonov@gmail.com>
2023-02-08 21:23:42 +0300

    Homework

---
+++ src/task06/t01main/container/Container.java
@@ -0,0 +1,28 @@
+package task06.t01main.container;
+
+import java.util.Random;
+
+public abstract class Container {
+
+    protected int waterDensity = waterDensity();
+    protected int height = doHeight();
+    private final int WATER_DENSITY_ONE = 1000;
+    private final int WATER_DENSITY_TWO = 2000;
+    protected Random random = new Random();
+
+    protected abstract double square(int diagonal);
+
+    protected abstract int waterMass(double volume);
+
+    protected double volume(double square){
+        return doHeight() * square;
+    }
+    protected int waterDensity(){
+        return (random.nextInt(2) == 0) ? WATER_DENSITY_ONE : WATER_DENSITY_TWO;
+    }
+
+    protected int doHeight(){
+        return random.nextInt(10, 100);
+    }
+
+}
